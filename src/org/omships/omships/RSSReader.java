4fc7132f1f36644f73153a80c56950c375b11c83
jimtahu <jimtahu@gmail.com>
2013-03-11 14:19:51 -0600

    added rss feed grabber

---
+++ src/org/omships/omships/RSSReader.java
@@ -0,0 +1,38 @@
+package org.omships.omships;
+
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Class reads RSS data.
+ * @author ITCuties
+ * From http://www.itcuties.com/android/how-to-write-android-rss-parser/
+ */
+public class RSSReader {
+	// Our class has an attribute which represents RSS Feed URL
+    private String rssUrl;
+    /**
+     * We set this URL with the constructor
+     */
+    public RSSReader(String rssUrl) {
+        this.rssUrl = rssUrl;
+    }
+    /**
+     * Get RSS items. This method will be called to get the parsing process result.
+     * @return
+     */
+    public List<RSSItem> getItems() throws Exception {
+        // At first we need to get an SAX Parser Factory object
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        // Using factory we create a new SAX Parser instance
+        SAXParser saxParser = factory.newSAXParser();
+        // We need the SAX parser handler object
+        RssParseHandler handler = new RssParseHandler();
+        // We call the method parsing our RSS Feed
+        saxParser.parse(rssUrl, handler);
+        // The result of the parsing process is being stored in the handler object
+        return handler.getItems();
+    }
+}
