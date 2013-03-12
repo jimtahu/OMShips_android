91d4fe14811e35e9865596fa9cb6c803ef094841
awp07 <andrew.w.pollard@gmail.com>
2013-03-12 17:16:30 -0500

    removed swiping methods

--- src/org/omships/omships/NewsFeedActivity.java
+++ src/org/omships/omships/NewsFeedActivity.java
@@ -17,8 +17,6 @@ import android.widget.ViewFlipper;
 
 public class NewsFeedActivity extends Activity {
 	
-	private ViewFlipper vf;
-	private int mCurrentLayoutState;
 	
 	class NewsItemClicked implements OnItemClickListener{
 		@Override
@@ -37,8 +35,6 @@ public class NewsFeedActivity extends Activity {
 		ListView rssList = (ListView) findViewById(R.id.newslist);
 		new FetchItems(this,rssList).execute("http://www.omships.org/rss/omsi_news.php");
 		rssList.setOnItemClickListener(new NewsItemClicked());
-		vf =(ViewFlipper)findViewById(R.id.view_flipper);
-		mCurrentLayoutState = 0;
 	}
 
 	@Override
@@ -47,106 +43,5 @@ public class NewsFeedActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		
 		return true;
-	}
-	
-//	@Override
-//	public boolean onTouchEvent(MotionEvent touchevent){
-//		switch (touchevent.getAction())
-//		{
-//		case MotionEvent.ACTION_DOWN:
-//		{
-//		lastX = touchevent.getX();
-//		break;
-//		}
-//		case MotionEvent.ACTION_UP:
-//		{
-//		float currentX = touchevent.getX();
-//		if (lastX < currentX)
-//		{
-//		if (vf.getDisplayedChild()==0) break;
-//		vf.setInAnimation(this, R.anim.infromleft);
-//		vf.setOutAnimation(this, R.anim.outtoright);
-//		vf.showNext();
-//		}
-//		if (lastX > currentX)
-//		{
-//		if (vf.getDisplayedChild()==1) break;
-//		vf.setInAnimation(this, R.anim.infromright);
-//		vf.setOutAnimation(this, R.anim.outtoleft);
-//		vf.showPrevious();
-//		}
-//		break;
-//		}
-//		}
-//		return false;
-//	}
-	
-	/*
-     * Switches the layout to the given constant ID as a parameter
-     * @param switchTo (should be 0 or a positive number)
-     */
-	public void switchLayoutStateTo(int switchTo){
-        while(mCurrentLayoutState != switchTo){
-                if(mCurrentLayoutState > switchTo){
-                        mCurrentLayoutState--;
-                        vf.setInAnimation(inFromLeftAnimation());
-                        vf.setOutAnimation(outToRightAnimation());
-                        vf.showPrevious();
-                } else {
-                        mCurrentLayoutState++;
-                        vf.setInAnimation(inFromRightAnimation());
-                        vf.setOutAnimation(outToLeftAnimation());
-                        vf.showNext();
-                }          
-               
-        };
-	}
-
-	/*
-	 * Animation methods
-	 */
-	protected Animation inFromRightAnimation() {
-		 
-        Animation inFromRight = new TranslateAnimation(
-                        Animation.RELATIVE_TO_PARENT, +1.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromRight.setDuration(500);
-        inFromRight.setInterpolator(new AccelerateInterpolator());
-        return inFromRight;
-	}
-
-	protected Animation outToLeftAnimation() {
-        Animation outtoLeft = new TranslateAnimation(
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, -1.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f);
-        outtoLeft.setDuration(500);
-        outtoLeft.setInterpolator(new AccelerateInterpolator());
-        return outtoLeft;
-	}
-
-	protected Animation inFromLeftAnimation() {
-        Animation inFromLeft = new TranslateAnimation(
-                        Animation.RELATIVE_TO_PARENT, -1.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f);
-        inFromLeft.setDuration(500);
-        inFromLeft.setInterpolator(new AccelerateInterpolator());
-        return inFromLeft;
-	}
-
-	protected Animation outToRightAnimation() {
-        Animation outtoRight = new TranslateAnimation(
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, +1.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.0f);
-        outtoRight.setDuration(500);
-        outtoRight.setInterpolator(new AccelerateInterpolator());
-        return outtoRight;
 	}
 }
