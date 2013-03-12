a191340715c4f9fe0cb17f32702cdf28821bd834
jimtahu <jimtahu@gmail.com>
2013-03-12 15:48:15 -0600

    Added camra view (needs work)

--- src/org/omships/omships/WebCam.java
+++ src/org/omships/omships/WebCam.java
@@ -1,9 +1,20 @@
 package org.omships.omships;
 
-public class WebCam {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class WebCam implements Parcelable{
 	String url;
 	String name;
 	long update;
+	public WebCam(){};
+	
+	public WebCam(Parcel source) {
+		this.name=source.readString();
+		this.url=source.readString();
+		this.update=source.readLong();
+	}
+
 	public String getUrl() {
 		return url;
 	}
@@ -22,4 +33,30 @@ public class WebCam {
 	public void setUpdate(long update) {
 		this.update = update;
 	}
+	public String toString(){
+		return this.name;
+	}
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.getName());
+		dest.writeString(this.getUrl());
+		dest.writeLong(this.getUpdate());
+	}
+	
+	public static final Parcelable.Creator<WebCam> CREATOR = new Parcelable.Creator<WebCam>() {
+		@Override
+		public WebCam createFromParcel(Parcel source) {
+			return new WebCam(source);
+		}
+		@Override
+		public WebCam[] newArray(int size) {
+			return new WebCam[size];
+		}
+	};
+	
 }//end class WebCam
