912d74e4dcece98aa288747c900c52f32e3278e9
jimtahu <jimtahu@gmail.com>
2013-03-12 15:49:44 +0000

    Extended itemview to show photo

--- src/org/omships/omships/NewsItemView.java
+++ src/org/omships/omships/NewsItemView.java
@@ -1,16 +1,51 @@
 package org.omships.omships;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 
 public class NewsItemView extends Activity {
 	RSSItem item;
+	
+	static class FetchImage extends AsyncTask<String, Void, Bitmap>{
+		ImageView photo;
+		
+		public FetchImage(ImageView photo) {
+			this.photo=photo;
+		}
+		
+		@Override
+		protected Bitmap doInBackground(String...urls) {
+			try {
+				Bitmap image = BitmapFactory.decodeStream(new URL(urls[0]).openStream());
+				return image;
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}//end doInBackground
+		
+		protected void onPostExecute(Bitmap result){
+			this.photo.setImageBitmap(result);
+		}
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +61,14 @@ public class NewsItemView extends Activity {
 		description.setText(item.getDescription());
 		TextView link = (TextView) findViewById(R.id.link);
 		link.setText(item.getLink());
-	}
+		if(item.getLink().endsWith(".jpg")){
+			ImageView photo = (ImageView) findViewById(R.id.photo);
+			new FetchImage(photo).execute(item.getLink());
+			photo.setVisibility(View.VISIBLE);
+			link.setVisibility(View.GONE);
+			link.setVisibility(View.GONE);
+		}
+	}//end onCreate
 
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
