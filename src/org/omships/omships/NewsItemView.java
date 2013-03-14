29c12a548542b3188f4984feaf7e88fe6de80cb7
jimtahu <jimtahu@gmail.com>
2013-03-13 23:06:53 -0600

    Updated ItemView, now includes webpage. Doesn't handle twitter very well, though

--- src/org/omships/omships/NewsItemView.java
+++ src/org/omships/omships/NewsItemView.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
@@ -23,18 +24,16 @@ public class NewsItemView extends Activity {
 		setupActionBar();
 		this.item = getIntent().getExtras().getParcelable("item");
 		this.setTitle(this.item.getTitle());
-		TextView title = (TextView) findViewById(R.id.title);
-		title.setText(item.getTitle());
 		TextView description = (TextView) findViewById(R.id.description);
 		description.setText(item.getDescription());
-		TextView link = (TextView) findViewById(R.id.link);
-		link.setText(item.getLink());
+		WebView webpage = (WebView) findViewById(R.id.webpage);
+		webpage.loadUrl(item.getLink());
 		if(item.getLink().endsWith(".jpg")){
 			ImageView photo = (ImageView) findViewById(R.id.photo);
 			new FetchImage(photo).execute(item.getLink());
 			photo.setVisibility(View.VISIBLE);
-			link.setVisibility(View.GONE);
-			link.setVisibility(View.GONE);
+			photo.setContentDescription(item.getDescription());
+			webpage.setVisibility(View.GONE);
 		}
 	}//end onCreate
 
