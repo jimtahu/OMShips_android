d3d5b9787f4894b3d70076dea228e16454a0f966
awp07 <andrew.w.pollard@gmail.com>
2013-03-14 11:25:22 -0500

    added pagerTabStrip, but without fragments inserted

--- src/org/omships/omships/MainActivity.java
+++ src/org/omships/omships/MainActivity.java
@@ -1,30 +1,83 @@
 package org.omships.omships;
-
+ 
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.TabActivity;
-import android.view.Menu;
-import android.view.View.OnClickListener;
-import android.widget.TabHost;
-
-public class MainActivity extends TabActivity {
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_main);
-
-	    TabHost mTabHost = getTabHost();
-	    
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator("TAB 1").setContent(R.id.textview1));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator("TAB 2").setContent(R.id.textview2));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test3").setIndicator("TAB 3").setContent(R.id.textview3));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test4").setIndicator("TAB 4").setContent(R.id.textview4));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test5").setIndicator("TAB 5").setContent(R.id.textview5));
-	    
-	    mTabHost.setCurrentTab(0);
-	}
-
-	
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+ 
+public class MainActivity extends FragmentActivity {
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter());
+    }
+ 
+    public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 5;
+ 
+        public SampleFragmentPagerAdapter() {
+            super(getSupportFragmentManager());
+        }
+ 
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+ 
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.create(position + 1);
+        }
+        
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position == 0){
+            	return "Map";}
+            else if(position == 1){
+            	return "News/PrayerRequests";}
+            else if(position == 2){
+            	return "Media";}
+            else if(position == 3){
+            	return "Webcams";}
+            else{
+            	return "Port Schedule";}
+        }
+    }
+ 
+    public static class PageFragment extends Fragment {
+        public static final String ARG_PAGE = "ARG_PAGE";
+ 
+        private int mPage;
+ 
+        public static PageFragment create(int page) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_PAGE, page);
+            PageFragment fragment = new PageFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+ 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mPage = getArguments().getInt(ARG_PAGE);
+        }
+ 
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_page, container, false);
+            TextView textView = (TextView) view;
+            textView.setText("Fragment #" + mPage);
+            return view;
+        }
+    }
 }
