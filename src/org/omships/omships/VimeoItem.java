c0f2e045fb4f73b77545493601b7e53e88716806
jimtahu <jimtahu@gmail.com>
2013-03-15 12:06:49 -0600

    forgot to add file to last commit

---
+++ src/org/omships/omships/VimeoItem.java
@@ -0,0 +1,18 @@
+package org.omships.omships;
+
+public class VimeoItem extends FeedItem {
+	public VimeoItem(FeedItem item){
+		this.setTitle(item.getTitle());
+		this.setDescription(item.getDescription());
+		this.setPubDate(item.getPubDate());
+		this.setLink(item.getLink());
+	}
+	@Override
+	public String getDescription() {
+		return super.getTitle();
+	}
+	@Override
+	public String toString() {
+		return this.getDescription();
+	}
+}//end class VimeoItem
