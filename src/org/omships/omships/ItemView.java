1cea90cbb3cf04f006e35a218d39536e5c9f1bb1
awp07 <andrew.w.pollard@gmail.com>
2013-03-14 20:46:13 -0500

    video playback with play/pause

--- src/org/omships/omships/ItemView.java
+++ src/org/omships/omships/ItemView.java
@@ -6,15 +6,19 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
 
 public class ItemView extends Activity {
 	RSSItem item;
+	WebView webpage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class ItemView extends Activity {
 		this.setTitle(this.item.getTitle());
 		TextView description = (TextView) findViewById(R.id.description);
 		description.setText(item.getDescription());
-		WebView webpage = (WebView) findViewById(R.id.webpage);
+		webpage = (WebView) findViewById(R.id.webpage);
 		ImageView photo = (ImageView) findViewById(R.id.photo);
 		if(item.isImage()){
 			new FetchImage(photo).execute(item.getLink());
@@ -34,7 +38,23 @@ public class ItemView extends Activity {
 			photo.setContentDescription(item.getDescription());
 			webpage.setVisibility(View.GONE);
 		}else{
-			webpage.loadUrl(item.getLink());
+			WebSettings webSettings = webpage.getSettings();
+			webSettings.setJavaScriptEnabled(true);
+			
+			//turns off plugins for all android versions
+			if(android.os.Build.VERSION.SDK_INT < 8)
+				webpage.getSettings().setPluginsEnabled(false);
+			else
+				webpage.getSettings().setPluginState(WebSettings.PluginState.OFF);
+			//end if/else statement
+			
+			String vidURL;
+			vidURL = item.getLink().substring(17, item.getLink().length());
+			if (savedInstanceState == null)
+			{
+				webpage.loadUrl("http://player.vimeo.com/video/"+vidURL+"?autoplay=1&portrait=0&title&byline");
+			}
+			
 		}//end if image
 	}//end onCreate
 
@@ -71,5 +91,21 @@ public class ItemView extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	//to preserve webview state
+	@Override
+	protected void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		//Save the state of the WebView
+		webpage.saveState(outState);
+	}
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState){
+		super.onRestoreInstanceState(savedInstanceState);
+		//restore the state of the WebView
+		webpage.restoreState(savedInstanceState);
+	}
 
 }
+
